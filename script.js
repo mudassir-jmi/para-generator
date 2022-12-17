f858fac026d0162fc71352c743698065f2f32497
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-12-17 18:26:13 +0530

    random words generator

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#numOfwords');
+const container = document.querySelector(".container");
+const generateWord = (n) => {
+    let text = "";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i = 0; i < n; i++) {
+      const random = (Math.random()*25).toFixed(0);
+      text += letters[random];
+    }
+    return text;
+}
+// console.log(generateWord(26));
+let numOfWords;
+const generatePara = () => {
+    numOfWords = Number(input.value);
+    const para = document.createElement("p");
+    let data = "";
+    for(let i = 0; i < numOfWords; i++) {
+        const randomNumber = (Math.random()*15).toFixed(0);
+        data += generateWord(randomNumber);
+        data += " ";
+    }
+    // console.log(data);
+    para.innerText = data;
+    para.setAttribute("class", "paras");
+    container.append(para);
+}
+
+
